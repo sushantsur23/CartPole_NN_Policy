359b1e32ce2563c33a89736d964f68a28f66f68c
sushantsur23 <sushant.sur23@gmail.com>
2022-07-12 12:21:25 +0530

    final policy updates

--- src/simple_policy_example.py
+++ src/simple_policy_example.py
@@ -1,11 +1,44 @@
 import gym
 import numpy as np
 import time
+from tqdm import tqdm
 
 env = gym.make('CartPole-v0')
 
-Observations = env.reset()
-print(Observations)
+# def basic_policy(angle):
+#     if angle<0: #falling left
+#         return 0 #move left
+#     else:
+#         return 1  #move right
 
-for _ in range(100):
-    env.render()
+def basic_policy(pole_angle):
+    if pole_angle<0: #falling left
+        return 0 #move left
+    return 1  #move right
+
+total_rewards = list()
+
+N_episodes = 200
+N_Steps = 200
+
+for episode in range(N_episodes):
+    rewards = 0
+    #cat position, cart velocity, pole angle, pole velocity
+    observation = env.reset()
+    pole_angle = observation[2]
+    for step in tqdm(range(N_Steps)):
+        env.render()
+        action = basic_policy(pole_angle)
+        observation, reward, done, info = env.step(action)
+        time.sleep(0.001)
+        rewards+= rewards
+        if done:   #fallen
+            break
+    total_rewards.append(rewards)
+stats = {
+    "mean":np.mean(total_rewards),
+    "std_dev":np.std(total_rewards),
+    "min": np.min(total_rewards),
+    "max":np.max(total_rewards)
+}
+print(stats)
